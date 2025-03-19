94850040b4349e56e701305e339c10406b177cfd
Tadi Mhizha <151214925+Tadi263@users.noreply.github.com>
2025-03-19 16:19:56 +0000

    completed fib sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let fib = [0, 1]
+    if (a < 0){
+      return "OOPS";
+    };
+     for (let i = 0; i < a; i++){
+       sum = fib[i] + fib[i + 1];
+       fib.push(sum);
+     };
+      return fib[a];
 };
 
 // Do not edit below this line
