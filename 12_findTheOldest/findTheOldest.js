5f719f3f684507474ecfe8b635a41980028d01f0
Tadi Mhizha <151214925+Tadi263@users.noreply.github.com>
2025-03-19 17:19:02 +0000

    completed get oldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {let lastGuyDeath = a.yearOfDeath || new Date().getFullYear();
+        let nextGuyDeath = b.yearOfDeath || new Date().getFullYear();
+              let lastGuy = lastGuyDeath - a.yearOfBirth
+                                    let nextGuy = nextGuyDeath - b.yearOfBirth
+        return lastGuy > nextGuy ? 1 : -1;}).reduce((accumulator, employee) => {
+accumulator = employee;
+return accumulator;},
+{})
 };
 
 // Do not edit below this line
